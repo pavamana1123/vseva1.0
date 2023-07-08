94baafe2b3eaa0c9337852be6a4ec3a2c3403a2a
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-07-08 09:15:34 +0530

    last

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 var cred = require("./cred.js")
 const sync = require("./sync.js")
 const axios = require('axios')
+const axios = require('natural')
 
 function getNextUsername(str) {
   const matches = str.match(/(\d+)$/);
