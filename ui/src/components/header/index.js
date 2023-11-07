961c4162b2262bc590bba32c64a8041194e67749
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-11-07 09:18:46 +0530

    super

--- ui/src/components/header/index.js
+++ ui/src/components/header/index.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './index.css';
 import _ from "../../_"
 import menu from "../../menu"
 
 
 function Header(props) {
-  var {children, hideMenus} = props
+  var {children} = props
 
   var [menuOpen, setMenuOpen] = useState(false);
   var [contextMenuOpen, setContextMenuOpen] = useState(false);
@@ -26,50 +26,22 @@ function Header(props) {
     setContextMenuOpen(false)
   }
 
-  const save =  _.getSave()
+  const save =  useRef(_.getSave())
 
   return (
     <div className="header-holder">
       <div className='header'>
-          {hideMenus?<div/>:<img src="img/header/menu.png" id="header-menu" onClick={toggleMenu}/>}
+          <img src="img/header/menu.png" id="header-menu" onClick={toggleMenu}/>
           <div className='header-text'>ISKCON Mysore Volunteering</div>
-          {hideMenus?<div/>:<img src="img/header/dots.png" id="header-dots" onClick={toggleContextMenu}/>}
+          <div id="header-dp-init" style={{background: save.current.name.color()}}>{_.getInitials(save.current.name)}</div>
       </div>
 
-      {!hideMenus?<div className={`header-menu ${menuOpen?'open':''}`}>
+      <div className={`header-menu ${menuOpen?'open':''}`}>
         <div className='header-menu-cont'>
-          <div className='header-user-det-cont'>
-            <div className='header-user-det-dp-cont'>
-              <div className='header-user-det-dp'>
-                {_.getInitials(save.name)}
-              </div>
-            </div>
-            <div className='header-user-det'>
-              <div className='header-user-det-name'>
-                {save.name}
-              </div>
-              <div className='header-user-det-id'>
-                {`@${save.username} | ${save.roleInfo.sort((r1, r2)=>{
-                  return r1.roleIndex>r2.roleIndex?-1:1
-                })[0].roleName}`}
-              </div>
-            </div>
-          </div>
-
           <div className='header-menu-items-cont'>
             <div className='header-menu-items'>
                 <hr className='menu-items-sep'/>
-                {
-                  menu.filter(m=>{
-                    return m.roles.indexOf(save.roleInfo[0].roleName)!=-1
-                  }).map(m=>{
-                    return (
-                      <a className={`header-menu-item ${window.location.pathname==m.url?'menu-selected':''}`} href={m.url} target='_self'>
-                        {m.title}
-                      </a>
-                   )
-                  })
-                }
+
             </div>
             <div className='header-settings-cont'>
                 <hr className='menu-items-sep'/>
@@ -83,11 +55,11 @@ function Header(props) {
           </div>
 
         </div>
-      </div>:null}
+      </div>
 
-      {!hideMenus?<div className={`header-contextmenu ${contextMenuOpen?'open':''}`}>
+      <div className={`header-contextmenu ${contextMenuOpen?'open':''}`}>
         {children}
-      </div>:null}
+      </div>
 
       <div className={`header-menuglass ${!menuOpen?'hide':''}`} onClick={closeMenu}>
       </div>
--- ui/src/strings.js
+++ ui/src/strings.js
@@ -38,4 +38,35 @@ String.prototype.toPhoneCase = function() {
     }
 }
 
+String.prototype.color = function() {
+  let sum = 0;
+  for (let i = 0; i < this.length; i++) {
+    sum += this.charCodeAt(i);
+  }
+  const lightMaterialColors = [
+    '#8BC34A', // Light Green
+    '#CDDC39', // Lime
+    '#FFEB3B', // Yellow
+    '#FFC107', // Amber
+    '#FF9800', // Orange
+    '#FF5722', // Deep Orange
+    '#03A9F4', // Light Blue
+    '#00BCD4', // Cyan
+    '#607D8B', // Blue Grey
+    '#9E9E9E', // Grey
+    '#F44336', // Red
+    '#2196F3', // Light Blue
+    '#4CAF50', // Green
+    '#E91E63', // Pink
+    '#673AB7', // Deep Purple
+    '#03A9F4', // Light Blue
+    '#4CAF50', // Green
+    '#2196F3', // Blue
+    '#607D8B', // Blue Grey
+    '#9E9E9E'  // Grey
+  ]
+
+  return lightMaterialColors[sum % lightMaterialColors.length];
+}
+
 
