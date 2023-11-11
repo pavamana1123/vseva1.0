005ef0d130fc0b309cf52aa830d71faf5101e57a
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-11-11 09:46:41 +0530

    users

--- ui/src/components/header/index.js
+++ ui/src/components/header/index.js
@@ -58,6 +58,17 @@ function Header(props) {
               <div className='header-userrole'>{user.role.name}</div>
             </div>
         </div>
+        <hr className='header-user-hr'/>
+
+        <div className='header-actions'>
+          <div className='header-action-label'>ACTIONS</div>
+          <div className='header-action-list'>
+            <div className='header-action-item'>Edit Profile</div>
+            <div className='header-action-item'>Add User</div>
+            <div className='header-action-item'>Notifications</div>
+          </div>
+
+        </div>
       </div>}
 
       <div className={`header-menu ${menuOpen?'open':''}`}>
